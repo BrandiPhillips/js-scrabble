bd63633ec55883892a391ac5902a878bef5bed43
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-16 15:33:17 -0800

    started setting up the pieces

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,35 @@
-var Scrabble = function() {};
-
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+// object constructor creating a Scrabble object:
+var Scrabble = function() {
+  this.word = word || "";
 };
 
+
+// This is adding a method to the Scrabble object:
+Scrabble.prototype.scoreWord = function(word) {
+  score = 0;
+  word.forEach(function(letter){
+    if (letter == "A"|| "E" || "I" || "O" || "U" || "L" || "N" || "R" || "S" || "T") {
+      score += 1;
+    } else if (letter == "D" || "G") {
+      score += 2;
+    } else if (letter == "B" || "C" || "M" || "P") {
+      score += 3;
+    } else if (letter == "F" || "H" || "V" || "W" || "Y") {
+      score += 4;
+    } else if (letter == "K") {
+      score += 5;
+    } else if (letter == "J" || "X") {
+      score += 8;
+    } else
+      score += 10;
+  });
+  return score;
+};
+
+var gameOne = new Scrabble;
+
+gameOne.word = "JAX"
+console.log(Scrabble.scoreWord(word));
+
+// what is this for?:
 module.exports = Scrabble;
