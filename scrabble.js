774549c9348eb3733f44d0555d4e7739ed6fe9ce
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-17 07:57:52 -0800

    score word is working

--- scrabble.js
+++ scrabble.js
@@ -1,24 +1,25 @@
 // object constructor creating a Scrabble object:
-var Scrabble = function() {
-  this.word = word || "";
-};
+var Scrabble = function() {};
 
 
 // This is adding a method to the Scrabble object:
 Scrabble.prototype.scoreWord = function(word) {
+  word = Array.from(word);
+
   score = 0;
+
   word.forEach(function(letter){
-    if (letter == "A"|| "E" || "I" || "O" || "U" || "L" || "N" || "R" || "S" || "T") {
+    if (letter == "A"|| letter == "E" || letter == "I" || letter == "O" || letter == "U" || letter == "L" || letter == "N" || letter == "R" || letter == "S" || letter == "T") {
       score += 1;
-    } else if (letter == "D" || "G") {
+    } else if (letter == "D" || letter == "G") {
       score += 2;
-    } else if (letter == "B" || "C" || "M" || "P") {
+    } else if (letter == "B" || letter == "C" || letter == "M" || letter == "P") {
       score += 3;
-    } else if (letter == "F" || "H" || "V" || "W" || "Y") {
+    } else if (letter == "F" || letter == "H" || letter == "V" || letter == "W" || letter == "Y") {
       score += 4;
     } else if (letter == "K") {
       score += 5;
-    } else if (letter == "J" || "X") {
+    } else if (letter == "J" || letter == "X") {
       score += 8;
     } else
       score += 10;
@@ -28,8 +29,8 @@ Scrabble.prototype.scoreWord = function(word) {
 
 var gameOne = new Scrabble;
 
-gameOne.word = "JAX"
-console.log(Scrabble.scoreWord(word));
+// word = "JAX"
+console.log(gameOne.scoreWord("JAX"));
 
 // what is this for?:
 module.exports = Scrabble;
